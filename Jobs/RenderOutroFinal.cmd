94f41e698c6e388dfeb1264b19948a359ca19ecb
AonoZan <aonozan@gmailcom>
2017-02-19 20:52:34 +0100

    Outro command line render scripts.

---
+++ Jobs/RenderOutroFinal.cmd
@@ -0,0 +1,4 @@
+blender -b ..//ForumVideosProject.blend -S "OutroComposing" -E BLENDER_RENDER -a -s 1 -e 100
+
+echo "Done rendering."
+pause
---
+++ Jobs/RenderOutroFrames.cmd
@@ -0,0 +1,4 @@
+blender -b ..//ForumVideosProject.blend -S "Outro" -E BLENDER_RENDER -a -s 1 -e 100
+
+echo "Done rendering."
+pause
